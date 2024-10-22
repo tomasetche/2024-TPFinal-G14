d200d887200be1faa2e871294ad7c89a7e9d97cb
tomasetche <163159362+tomasetche@users.noreply.github.com>
2024-10-22 10:00:54 -0300

    etche.10.22.9
    j

---
+++ FrontPosta/src/app/components/publicacion.js
@@ -0,0 +1,17 @@
+"use client"
+import styles from "./publicacion.module.css"
+
+import React from 'react';
+
+
+const Publicacion = ({ imageUrl, precio, productName }) => {
+  return (
+    <div className={styles.cuadro}>
+      <img src={imageUrl} alt={productName} className={styles.image} />
+      <div className={styles.precio}>{precio}</div>
+      <div className={styles.productName}>{productName}</div>
+    </div>
+  );
+};
+
+export default Publicacion;
